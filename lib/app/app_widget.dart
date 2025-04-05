d8ebc31d155045dec1e581c03bee01f9875ec944
Stenio Oliveira <steniooliv@gmail.com>
2025-04-05 15:45:48 -0300

    feat: add responsive util

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Assets Management',
-      theme: ThemeData(),
-      home: const Scaffold(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      builder: (context, child) => child ?? const SizedBox.shrink(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Assets Management',
+        theme: ThemeData(),
+        home: const Scaffold(body: Column()),
+      ),
     );
   }
 }
