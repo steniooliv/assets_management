e2deb49cf5f8a33a77d5edd716fe04fd279fb9ef
Stenio Oliveira <steniooliv@gmail.com>
2025-04-05 16:22:51 -0300

    feat: add custom icons

---
+++ lib/app/core/theme/icons.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+
+class AIcons {
+  AIcons._();
+
+  static const String _fontFamily = 'TractianIcons';
+
+  static const IconData unit = IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData location = IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData asset = IconData(0xe902, fontFamily: _fontFamily);
+  static const IconData component = IconData(0xe903, fontFamily: _fontFamily);
+  static const IconData alert = IconData(0xe904, fontFamily: _fontFamily);
+  static const IconData boltOutline = IconData(0xe905, fontFamily: _fontFamily);
+  static const IconData bolt = IconData(0xe906, fontFamily: _fontFamily);
+  static const IconData circle = IconData(0xe907, fontFamily: _fontFamily);
+  static const IconData search = IconData(0xe908, fontFamily: _fontFamily);
+}
